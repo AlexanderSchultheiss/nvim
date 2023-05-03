1ffca34ffb3380e9ecb8c82acdbc57f1eff3d232
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-05-03 21:32:55 +0200

    configu: use clippy with rust-analyzer

---
+++ lua/config/rust-analyzer.lua
@@ -0,0 +1,17 @@
+require("lspconfig").rust_analyzer.setup({
+  settings = {
+    ["rust-analyzer"] = {
+      checkOnSave = {
+        allFeatures = true,
+        overrideCommand = {
+          "cargo",
+          "clippy",
+          "--workspace",
+          "--message-format=json",
+          "--all-targets",
+          "--all-features",
+        },
+      },
+    },
+  },
+})
