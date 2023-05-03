72be9f6971ac8540a3bb258b4bc300deb1514d26
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-05-03 13:48:51 +0200

    plugin: add texlab and configure vimtex keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,3 +22,27 @@ end, { desc = "update crates" })
 -- Change keymap of Lazy to 'L'
 vim.keymap.del("n", "<leader>l")
 map("n", "<leader>L", "<cmd>:Lazy<cr>", { desc = "Lazy" })
+
+-- vimtex
+map("n", "<leader>v[[", "<Plug>(vimtex-[[)", { desc = "section vimtex-[[" })
+map("n", "<leader>v]]", "<Plug>(vimtex-]])", { desc = "section vimtex-]]" })
+map("n", "<leader>v[]", "<Plug>(vimtex-[])", { desc = "section vimtex-[]" })
+map("n", "<leader>v][", "<Plug>(vimtex-][)", { desc = "section vimtex-][" })
+
+map("n", "<leader>v[m", "<Plug>(vimtex-[m)", { desc = "env. vimtex-[m" })
+map("n", "<leader>v[M", "<Plug>(vimtex-[M)", { desc = "env. vimtex-[M" })
+map("n", "<leader>v]m", "<Plug>(vimtex-]m)", { desc = "env. vimtex-]m" })
+map("n", "<leader>v]M", "<Plug>(vimtex-]M)", { desc = "env. vimtex-]M" })
+
+map("n", "<leader>v[n", "<Plug>(vimtex-[n)", { desc = "math vimtex-[n" })
+map("n", "<leader>v[N", "<Plug>(vimtex-[N)", { desc = "math vimtex-[N" })
+map("n", "<leader>v]n", "<Plug>(vimtex-]n)", { desc = "math vimtex-]n" })
+map("n", "<leader>v]N", "<Plug>(vimtex-]N)", { desc = "math vimtex-]N" })
+
+map("n", "<leader>v[r", "<Plug>(vimtex-[r)", { desc = "frame vimtex-[r" })
+map("n", "<leader>v[R", "<Plug>(vimtex-[R)", { desc = "frame vimtex-[R" })
+map("n", "<leader>v]r", "<Plug>(vimtex-]r)", { desc = "frame vimtex-]r" })
+map("n", "<leader>v]R", "<Plug>(vimtex-]R)", { desc = "frame vimtex-]R" })
+
+map("n", "<leader>v[*", "<Plug>(vimtex-[*)", { desc = "comment vimtex-[*" })
+map("n", "<leader>v]*", "<Plug>(vimtex-]*)", { desc = "comment vimtex-]*" })
