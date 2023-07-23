9628abc60c49190c4432e9f2341b79c8407ed07e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-07-23 17:29:59 +0200

    configuration: rust-tools keymap

--- lua/config/rust-tools.lua
+++ lua/config/rust-tools.lua
@@ -63,6 +63,19 @@ local opts = {
 
 require("rust-tools").setup(opts)
 
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>ra", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
 local lspkind_comparator = function(conf)
   local lsp_types = require("cmp.types").lsp
   return function(entry1, entry2)
