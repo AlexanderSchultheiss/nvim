deea9e20f1ed032be84692e184438660912d841b
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-09-21 10:55:16 +0200

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,4 +5,14 @@ return {
     priority = 1000,
     opts = {},
   },
+  {
+    "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "briones-gabriel/darcula-solid.nvim",
+    lazy = false,
+    priority = 100,
+  },
 }
