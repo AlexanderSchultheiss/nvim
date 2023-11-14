98af340bd513cf9b52634627eedeb8c31a8702d1
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-11-14 17:16:24 +0100

    config: add mason config to ensure installed

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "codelldb",
+        "lua-language-server",
+        "jedi-language-server",
+        "python-lsp-server",
+        "shfmt",
+        "stylua",
+        "json-lsp",
+      },
+    },
+  },
+}
