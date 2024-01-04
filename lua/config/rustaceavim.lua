96a3a5652afe7fffa5bb4ae885a99b19b06910e2
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-01-04 16:05:14 +0100

    rust: add debugging capabilities

--- lua/config/rustaceavim.lua
+++ lua/config/rustaceavim.lua
@@ -1,18 +1,10 @@
 vim.g.rustaceanvim = function()
   -- Update this path
-  local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
+  local extension_path = vim.env.HOME .. ".vscode-oss/extensions/vadimcn.vscode-lldb-1.10.0-universal/"
   local codelldb_path = extension_path .. "adapter/codelldb"
   local liblldb_path = extension_path .. "lldb/lib/liblldb"
-  local this_os = vim.uv.os_uname().sysname
 
-  -- The path is different on Windows
-  if this_os:find("Windows") then
-    codelldb_path = extension_path .. "adapter\\codelldb.exe"
-    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-  else
-    -- The liblldb extension is .so for Linux and .dylib for MacOS
-    liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-  end
+  liblldb_path = liblldb_path .. (".so" or ".dylib")
 
   local cfg = require("rustaceanvim.config")
   return {
@@ -21,3 +13,5 @@ vim.g.rustaceanvim = function()
     },
   }
 end
+
+vim.g.rustaceanvim()
--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -49,6 +49,15 @@ return {
     end,
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "rust",
+      })
+    end,
+  },
+  {
     "saecki/crates.nvim",
     dependencies = "hrsh7th/nvim-cmp",
     ft = { "rust", "toml" },
