427cb6adc79ad0cdc60362728446b829b5447708
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-01-04 16:09:14 +0100

    rust: update crates configuration

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -59,12 +59,10 @@ return {
   },
   {
     "saecki/crates.nvim",
-    dependencies = "hrsh7th/nvim-cmp",
+    dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp" },
     ft = { "rust", "toml" },
-    config = function(_, opts)
-      local crates = require("crates")
-      crates.setup(opts)
-      crates.show()
+    config = function()
+      require("crates").setup()
     end,
   },
   {
