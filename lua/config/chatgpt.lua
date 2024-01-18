3f6e966d0702bc7c09402425c79638e208117dc5
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-01-18 13:30:37 +0100

    gpt: update configuration

--- lua/config/chatgpt.lua
+++ lua/config/chatgpt.lua
@@ -8,17 +8,23 @@ local defaults = {
       accept = "<C-y>",
       toggle_diff = "<C-d>",
       toggle_settings = "<C-o>",
+      toggle_help = "<C-h>",
       cycle_windows = "<Tab>",
       use_output_as_input = "<C-i>",
     },
   },
   chat = {
     welcome_message = WELCOME_MESSAGE,
-    loading_text = "Loading ChatGPT, please wait ...",
+    loading_text = "Loading, please wait ...",
     question_sign = "", -- 🙂
     answer_sign = "ﮧ", -- 🤖
+    border_left_sign = "",
+    border_right_sign = "",
     max_line_length = 120,
     sessions_window = {
+      active_sign = "  ",
+      inactive_sign = "  ",
+      current_line_sign = "",
       border = {
         style = "rounded",
         text = {
@@ -30,7 +36,7 @@ local defaults = {
       },
     },
     keymaps = {
-      close = { "<C-c>" },
+      close = "<C-c>",
       yank_last = "<C-y>",
       yank_last_code = "<C-k>",
       scroll_up = "<C-u>",
@@ -43,10 +49,12 @@ local defaults = {
       select_session = "<Space>",
       rename_session = "r",
       delete_session = "d",
-      draft_message = "<C-d>",
+      draft_message = "<C-r>",
       edit_message = "e",
       delete_message = "d",
       toggle_settings = "<C-o>",
+      toggle_sessions = "<C-p>",
+      toggle_help = "<C-h>",
       toggle_message_role = "<C-r>",
       toggle_system_role_open = "<C-s>",
       stop_generating = "<C-x>",
@@ -114,10 +122,23 @@ local defaults = {
     max_visible_lines = 20,
   },
   settings_window = {
+    setting_sign = "  ",
     border = {
       style = "rounded",
       text = {
         top = " Settings ",
+      },
+    },
+    win_options = {
+      winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+    },
+  },
+  help_window = {
+    setting_sign = "  ",
+    border = {
+      style = "rounded",
+      text = {
+        top = " Help ",
       },
     },
     win_options = {
@@ -145,5 +166,9 @@ local defaults = {
   actions_paths = {},
   show_quickfixes_cmd = "Trouble quickfix",
   predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv",
+  highlights = {
+    help_key = "@symbol",
+    help_description = "@comment",
+  },
 }
 require("chatgpt").setup(defaults)
