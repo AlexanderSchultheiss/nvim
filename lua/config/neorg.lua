11833334079ae4977b8d4e08681c9757157bda3c
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-05-30 15:52:52 +0200

    neorg: add completion

--- lua/config/neorg.lua
+++ lua/config/neorg.lua
@@ -2,6 +2,12 @@ require("neorg").setup({
   load = {
     ["core.defaults"] = {}, -- Loads default behaviour
     ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    ["core.summary"] = {},
+    ["core.completion"] = { -- Manages Neorg workspaces
+      config = {
+        engine = "nvim-cmp",
+      },
+    },
     ["core.dirman"] = { -- Manages Neorg workspaces
       config = {
         workspaces = {
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -8,4 +8,11 @@ return {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "neorg" } }))
+    end,
+  },
 }
