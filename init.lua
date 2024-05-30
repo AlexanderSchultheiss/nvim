de2d61d23dd5cb27c67497045681ba695d500b24
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-05-30 15:39:40 +0200

    image: add plugin

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ vim.cmd("source ~/.config/nvim/vim/vimtex.vim")
 vim.cmd("source ~/.config/nvim/vim/neomake.vim")
 require("config.java")
 require("config.neorg")
+require("config.image")
 
 -- colorscheme
 -- vim.cmd([[colorscheme tokyonight]])
