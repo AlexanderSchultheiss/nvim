7c6ec32608df4adbdb798b5234112694f3459f31
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-07-14 12:23:59 +0200

    config: remove neorg calendar

--- lua/config/neorg.lua
+++ lua/config/neorg.lua
@@ -3,7 +3,6 @@ require("neorg").setup({
     ["core.defaults"] = {}, -- Loads default behaviour
     ["core.concealer"] = {}, -- Adds pretty icons to your documents
     ["core.summary"] = {},
-    ["core.ui.calendar"] = {},
     ["core.integrations.image"] = {},
     ["core.latex.renderer"] = {},
     ["core.journal"] = { -- Manages Neorg workspaces
